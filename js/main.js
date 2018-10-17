e54e58de6dfed52fa106bd8fcc8bf1e94a8abe4e
Eric Blackham <erict.blackham@gmail.com>
2018-10-16 21:54:55 -0600

    Pop-Out menu & back to top link now working

--- js/main.js
+++ js/main.js
@@ -1,1 +1,34 @@
+function scrollToTop() {
+  window.scrollTo({
+    top: 0, // could be negative value
+    left: 0,
+    behavior: "smooth"
+  });
+}
 
+document.querySelector("#topBtn").addEventListener("click", scrollToTop);
+document.querySelector("#menuBtn").addEventListener("click", slideOutMenu);
+
+function slideOutMenu() {
+  console.log("menu slide call");
+  var menu = document.querySelector("#slide-out-menu");
+  var menuBtn = document.querySelector("#menuBtn");
+  var menuLines = document.querySelectorAll(".menuLine");
+  if (menu.classList[1] == "show") {
+    menu.classList.remove("show");
+    menuBtn.classList.remove("menu-x");
+    menuLines.forEach(line => {
+      line.classList.remove("menuLine-hide");
+    });
+  } else {
+    menu.classList.add("show");
+    menuBtn.classList.add("menu-x");
+    menuLines.forEach(line => {
+      line.classList.add("menuLine-hide");
+    });
+  }
+
+  //   if (menuBtn.classList[0] == "menu-x") {
+  //   } else {
+  //   }
+}
