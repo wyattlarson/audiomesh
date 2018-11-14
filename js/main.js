b36543c14e7ee09a5669c75645866ce7a2b2d423
Eric Blackham <erict.blackham@gmail.com>
2018-11-14 12:35:31 -0700

    friendlier for larger screens

--- js/main.js
+++ js/main.js
@@ -1,30 +1,32 @@
 function scrollToTop() {
+  console.log('scrolled yo');
+
   window.scrollTo({
     top: 0, // could be negative value
     left: 0,
-    behavior: "smooth"
+    behavior: 'smooth'
   });
 }
 
-document.querySelector("#topBtn").addEventListener("click", scrollToTop);
-document.querySelector("#menuBtn").addEventListener("click", slideOutMenu);
+document.querySelector('#topBtn').addEventListener('click', scrollToTop);
+document.querySelector('#menuBtn').addEventListener('click', slideOutMenu);
 
 function slideOutMenu() {
-  console.log("menu slide call");
-  var menu = document.querySelector("#slide-out-menu");
-  var menuBtn = document.querySelector("#menuBtn");
-  var menuLines = document.querySelectorAll(".menuLine");
-  if (menu.classList[1] == "show") {
-    menu.classList.remove("show");
-    menuBtn.classList.remove("menu-x");
+  console.log('menu slide call');
+  var menu = document.querySelector('#slide-out-menu');
+  var menuBtn = document.querySelector('#menuBtn');
+  var menuLines = document.querySelectorAll('.menuLine');
+  if (menu.classList[1] == 'show') {
+    menu.classList.remove('show');
+    menuBtn.classList.remove('menu-x');
     menuLines.forEach(line => {
-      line.classList.remove("menuLine-hide");
+      line.classList.remove('menuLine-hide');
     });
   } else {
-    menu.classList.add("show");
-    menuBtn.classList.add("menu-x");
+    menu.classList.add('show');
+    menuBtn.classList.add('menu-x');
     menuLines.forEach(line => {
-      line.classList.add("menuLine-hide");
+      line.classList.add('menuLine-hide');
     });
   }
 
@@ -33,14 +35,14 @@ function slideOutMenu() {
   //   }
 }
 function showSignUp() {
-  document.querySelector("#signUpBody").classList.add("showSignup");
+  document.querySelector('#signUpBody').classList.add('showSignup');
 }
 function closeSignUp() {
-  document.querySelector("#signUpBody").classList.remove("showSignup");
+  document.querySelector('#signUpBody').classList.remove('showSignup');
 }
 function showLogin() {
-  document.querySelector("#loginBody").classList.add("showSignup");
+  document.querySelector('#loginBody').classList.add('showSignup');
 }
 function closeLogin() {
-  document.querySelector("#loginBody").classList.remove("showSignup");
+  document.querySelector('#loginBody').classList.remove('showSignup');
 }
